691be5d36004ce4b022d39464a8efde20d946308
David Ordás <3125580+davorpa@users.noreply.github.com>
2023-07-13 23:58:39 +0200

    Agregar PersistenciaException como forma de encapsular los errores producidos en la capa de persistencia.

---
+++ centro-medico-v2/src/persistencia/PersistenciaException.java
@@ -0,0 +1,18 @@
+package persistencia;
+
+/**
+ * Encapsula los errores producidos en la capa de persistencia.
+ */
+public class PersistenciaException extends RuntimeException {
+    public PersistenciaException(String message) {
+        super(message);
+    }
+
+    public PersistenciaException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PersistenciaException(Throwable cause) {
+        super(cause);
+    }
+}
